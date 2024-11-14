e0bca37a2cae97d248f5b3bb848989d5d2b9b7f1
Sanjeev <sanjeevkumar14102003@gmail.com>
2024-11-15 00:31:55 +0530

    Day 3 Completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to 30 Days of Flutter"),
+        ),
+      ),
+    );
+  }
+}
